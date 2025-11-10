2f1d129856f950215569eb5a162207dd82f8dc14
asadrehman1 <asadrehman7890661@gmail.com>
2025-11-09 20:40:00 -0800

    test: add Jest setup/teardown hooks

--- components/Home.test.tsx
+++ components/Home.test.tsx
@@ -2,13 +2,37 @@ import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Home from "./Home";
 
-
 function sum(a: number, b: number) {
   return a + b;
 }
+let data = [];
+beforeAll(() => {
+  console.log("beforeAll");
+  data = [1, 2, 3, 4, 5];
+  console.log(data);
+});
+
+beforeEach(() => {
+  console.log("beforeEach");
+  data = [1, 2, 3, 4, 5];
+  console.log(data);
+});
+
+afterEach(() => {
+  console.log("afterEach");
+  data = [1, 2, 3, 4, 5];
+  console.log(data);
+});
+
+afterAll(() => {
+  console.log("afterAll");
+  data = [1, 2, 3, 4, 5];
+  console.log(data);
+});
+
 test("adds 1+2 to equal 3", () => {
-  expect(sum(1,2)).toBe(3);
-  expect(sum(3,3)).not.toBe(4);
+  expect(sum(1, 2)).toBe(3);
+  expect(sum(3, 3)).not.toBe(4);
 });
 
 test("objects are equal", () => {
@@ -19,14 +43,14 @@ test("objects are equal", () => {
 
 test("objects assign new properties", () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const obj1:any = { a: 1, b: 2 };
+  const obj1: any = { a: 1, b: 2 };
   obj1["c"] = 3;
   expect(obj1).toEqual({ a: 1, b: 2, c: 3 });
 });
 
 test('There is a "stop" in Christoph', () => {
   expect("Christoph").toMatch(/stop/);
-})
+});
 
 function getResponse() {
   return new Promise((resolve) => {
